733a2d93937a7fa1ad85bcf633a871e30a70eed2
jgawli <jyoti.gawli@gmail.com>
2016-06-28 22:28:54 -0400

    Update NamedEntityFinderTest.java

--- src/test/java/com/nlp/NamedEntityFinderTest.java
+++ src/test/java/com/nlp/NamedEntityFinderTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -41,6 +43,20 @@ public class NamedEntityFinderTest {
                 "The term \"First World War\" was first used in September 1914 by the German philosopher Ernst Haeckel, who claimed that \"there is no doubt that the course and character of the feared 'European War' ... will become the first world war in the full sense of the word.\"\n" +
                         "\n");
         assertEquals("2 nouns were not identified as expected from first sentence", xml.getSentences().get(0).getNoun().size(), 2);
+    }
+
+    @Test
+    public void processConcurrentFiles() throws Exception {
+        List<Future<SentenceXml>> xml = finder.processAllFilesForNLP(10);
+        assertNotNull(xml);
+        assertEquals("10 files were not processed", xml.size(), 10);
+        SentenceXml sentenceXmlFirstFile = xml.get(0).get();
+        assertEquals("4 sentences were not found in first SentenceXml as expected",
+                sentenceXmlFirstFile.getSentences().size(), 4);
+        Text firstSentence = sentenceXmlFirstFile.getSentences().get(0);
+        assertEquals("2 nouns were not found in first sentence as expected", firstSentence.getNoun().size(), 2);
+        assertEquals("First file name processed is incorrect", firstSentence.getDocId(), "d01.txt");
+
     }
 
 }
